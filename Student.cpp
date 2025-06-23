780081209a5d468c30b4b45452cd19345c5169fa
Do Ngo <bahuynh619@gmail.com>
2025-06-23 20:10:49 +0700

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,3 +1,4 @@
+#01
 #include <iostream>
 
 using namespace std;
@@ -22,4 +23,36 @@ int main() {
     Student s1;
     s1.getInformation();
     s1.display();
+}
+
+
+
+
+#02
+#include <iostream>
+
+using namespace std;
+
+class Student {
+    private:
+    string name;
+    int age;
+    public:
+    Student(string name, int age){
+        this->name=name;
+        this->age=age;
+    }
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+
+};
+
+int main() {
+    Student s1("Long", 24);
+    Student s2("Kien", 29);
+    s1.display();
+    s2.display();
+    return 0;
 }
